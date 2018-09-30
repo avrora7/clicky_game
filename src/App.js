cb27c31e3f36c297368a0f53154e971b661c2ea6
Elizabeth <avrora7@yahoo.com>
2018-09-29 20:05:23 -0700

    working code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
+// import Title from "./components/Title";
 import friends from "./friends.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    selections: [],
+    max: 0,
+    score: 0
   };
 
   checkFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    let selections = this.state.selections;
+    if (selections.indexOf(id) > -1) {
+
+      this.state.max = this.state.max < this.state.score ? this.state.score : this.state.max;
+      this.state.score = 0;
+      selections = [];
+    } else {
+      this.state.score++;
+      selections.push(id);
+    }
+    const friends = this.state.friends;
+
+    for (let i = friends.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [friends[i], friends[j]] = [friends[j], friends[i]];
+    }
+    this.setState({ friends, selections });
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="text-center">Clicky Game!</h1>
+            <p className="lead text-center">Click on an image to start the game and to earn points, but do not click on any image more than once!</p>
+          </div>
+          <div className="navbar-inner">
+        <h1> <span className="navbar-brand " >Score: {this.state.score} &nbsp;&nbsp;&nbsp;&nbsp; Top Score: {this.state.max}</span></h1>
+          </div>
+        </div>
         {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.checkFriend}
             id={friend.id}
             key={friend.id}
-            name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
         ))}
       </Wrapper>
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,26 +2,10 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card">
+  <div className="float-left card" onClick={() => props.removeFriend(props.id)}>
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
-    <span onClick={() => props.checkFriend(props.id)} className="remove">
-      𝘅
-    </span>
   </div>
 );
 
